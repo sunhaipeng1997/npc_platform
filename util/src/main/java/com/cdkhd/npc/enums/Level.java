05aa6a4e48e4bb3758f528b74545548ec97b2c39
really <renfaxin@qq.com>
2019-12-25 15:18:56 +0800

    枚举修改提交

--- util/src/main/java/com/cdkhd/npc/enums/Level.java
+++ util/src/main/java/com/cdkhd/npc/enums/Level.java
@@ -2,11 +2,46 @@ package com.cdkhd.npc.enums;
 
 public enum Level {
     //用户的身份等级（eg县代表 or 镇代表）
-    TOWN(Byte.valueOf("1")), AREA(Byte.valueOf("2"));
+    TOWN((byte)1,"镇"),
+    AREA((byte)2,"区");
 
-    public final Byte value;
+    private Byte value;
+    private String name;
 
-    Level(Byte v) {
-        this.value = v;
+    Level(Byte value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    private byte value() {
+        return this.value;
+    }
+
+    public byte getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Byte getValue(String name) {
+        Level[] levels = values();
+        for (Level level : levels) {
+            if (level.name().equals(name)) {
+                return level.value();
+            }
+        }
+        return null;
+    }
+
+    public static String getName(byte value) {
+        Level[] levels = values();
+        for (Level level : levels) {
+            if (level.value() == value) {
+                return level.getName();
+            }
+        }
+        return null;
     }
 }
