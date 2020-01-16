9709d4dcb507a70d18096d7e384b68d35c27313c
really <renfaxin@qq.com>
2020-01-16 14:36:29 +0800

    后端代码提交

--- repository/src/main/java/com/cdkhd/npc/repository/member_house/StudyTypeRepository.java
+++ repository/src/main/java/com/cdkhd/npc/repository/member_house/StudyTypeRepository.java
@@ -2,7 +2,7 @@ package com.cdkhd.npc.repository.member_house;
 
 import com.cdkhd.npc.entity.StudyType;
 import com.cdkhd.npc.repository.base.BaseRepository;
-import com.cdkhd.npc.vo.PageVo;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 
@@ -18,14 +18,14 @@ public interface StudyTypeRepository extends BaseRepository<StudyType> {
 
     StudyType findByNameAndLevelAndAreaUidAndIsDelFalse(String name, Byte level, String uid);
 
-    @Query(value = "select max(type.sequence) from PerformanceType type")
+    @Query(value = "select max(type.sequence) from StudyType type")
     Integer findMaxSequence();
 
-    @Query(value = "select type from PerformanceType as type where type.sequence < ?1 order by type.sequence desc ")
-    PageVo<StudyType> findBySequenceDesc(Integer sequence, Pageable page);
+    @Query(value = "select type from StudyType as type where type.sequence < ?1 order by type.sequence desc ")
+    Page<StudyType> findBySequenceDesc(Integer sequence, Pageable page);
 
-    @Query(value = "select type from PerformanceType as type where type.sequence > ?1 order by type.sequence asc ")
-    PageVo<StudyType> findBySequenceAsc(Integer sequence, Pageable page);
+    @Query(value = "select type from StudyType as type where type.sequence > ?1 order by type.sequence asc ")
+    Page<StudyType> findBySequenceAsc(Integer sequence, Pageable page);
 
     List<StudyType> findByLevelAndTownUidAndIsDelFalse(Byte level, String uid);
 
