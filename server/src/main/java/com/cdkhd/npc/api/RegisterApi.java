dfcfca577b7870e07c06c958bb480ee67be192d1
Allen Lee <liyalin.iui@gmail.com>
2020-03-27 20:54:38 +0800

    继续完善通知、登录、推送消息、注册模块

--- server/src/main/java/com/cdkhd/npc/api/RegisterApi.java
+++ server/src/main/java/com/cdkhd/npc/api/RegisterApi.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/member_house_mobile/register")
+@RequestMapping("/api/mobile/register")
 public class RegisterApi {
 
     private RegisterService registerService;
