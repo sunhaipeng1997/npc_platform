c21f9e9c5a9d81c9467f8210bb12d6b556624c7d
liyang <liyang96615@163.com>
2020-05-07 19:29:59 +0800

    撤回建议

--- util/src/main/java/com/cdkhd/npc/enums/SuggestionStatusEnum.java
+++ util/src/main/java/com/cdkhd/npc/enums/SuggestionStatusEnum.java
@@ -3,6 +3,7 @@ package com.cdkhd.npc.enums;
 public enum SuggestionStatusEnum {
     /**
      * ---------建议状态
+     已撤回	0
      未提交	1
      已提交审核	2
      已提交政府	3
@@ -13,6 +14,7 @@ public enum SuggestionStatusEnum {
      自行办理	8
      审核失败	-1
      */
+    HAS_BEEN_REVOKE((byte)0, "已撤回"),
     NOT_SUBMITTED((byte)1, "未提交"),
     SUBMITTED_AUDIT((byte)2, "待审核"),
     SUBMITTED_GOVERNMENT((byte)3, "已提交政府"),
