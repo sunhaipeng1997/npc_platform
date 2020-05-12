963b01dfdb0340891c9a318561ebd4bc3adec079
really <renfaxin@qq.com>
2020-05-12 11:48:20 +0800

    提意见，根据后台设置过滤代表

--- member_house_mobile/src/main/java/com/cdkhd/npc/service/impl/NpcMemberServiceImpl.java
+++ member_house_mobile/src/main/java/com/cdkhd/npc/service/impl/NpcMemberServiceImpl.java
@@ -8,6 +8,7 @@ import com.cdkhd.npc.entity.vo.CommentVo;
 import com.cdkhd.npc.entity.vo.MemberUnitVo;
 import com.cdkhd.npc.entity.vo.NpcMemberVo;
 import com.cdkhd.npc.enums.LevelEnum;
+import com.cdkhd.npc.enums.NpcMemberRoleEnum;
 import com.cdkhd.npc.enums.StatusEnum;
 import com.cdkhd.npc.repository.base.*;
 import com.cdkhd.npc.service.NpcMemberService;
@@ -42,14 +43,20 @@ public class NpcMemberServiceImpl implements NpcMemberService {
 
     private AccountRepository accountRepository;
 
+    private SystemSettingRepository systemSettingRepository;
+
+    private NpcMemberRoleRepository npcMemberRoleRepository;
+
 
     @Autowired
-    public NpcMemberServiceImpl(NpcMemberRepository npcMemberRepository, TownRepository townRepository, AreaRepository areaRepository, NpcMemberGroupRepository npcMemberGroupRepository, AccountRepository accountRepository) {
+    public NpcMemberServiceImpl(NpcMemberRepository npcMemberRepository, TownRepository townRepository, AreaRepository areaRepository, NpcMemberGroupRepository npcMemberGroupRepository, AccountRepository accountRepository, SystemSettingRepository systemSettingRepository, NpcMemberRoleRepository npcMemberRoleRepository) {
         this.npcMemberRepository = npcMemberRepository;
         this.townRepository = townRepository;
         this.areaRepository = areaRepository;
         this.npcMemberGroupRepository = npcMemberGroupRepository;
         this.accountRepository = accountRepository;
+        this.systemSettingRepository = systemSettingRepository;
+        this.npcMemberRoleRepository = npcMemberRoleRepository;
     }
 
     /**
@@ -61,30 +68,48 @@ public class NpcMemberServiceImpl implements NpcMemberService {
     public RespBody relationOfNpcMember(MobileUserDetailsImpl userDetails, LevelDto levelDto) {
         RespBody body = new RespBody();
         List<MemberUnitVo> MemberUnitVos = Lists.newArrayList();
+        SystemSetting systemSetting;
+        Account account = accountRepository.findByUid(userDetails.getUid());
+        NpcMemberRole specialMan = npcMemberRoleRepository.findByKeyword(NpcMemberRoleEnum.SPECIAL_MAN.getKeyword());
         if (levelDto.getLevel().equals(LevelEnum.TOWN.getValue())){
-            //如果是镇上，就查询小组
             //如果传了需要查询的镇的小组那么就按照产过来的查询，如果没有传过来，那么就按照当前登录人所在的镇来查询
             String townUid = userDetails.getTown().getUid();
+            //开关判断能否给非本小组的代表提意见
+            systemSetting  = systemSettingRepository.findByLevelAndTownUid(LevelEnum.TOWN.getValue(),townUid);
+            //如果是镇上，就查询小组
             Town town = townRepository.findByUid(townUid);
             Set<NpcMemberGroup> groupList = town.getNpcMemberGroups();
             for (NpcMemberGroup npcMemberGroup : groupList) {//每个小组里面的代表信息
-                MemberUnitVo memberUnitVo = MemberUnitVo.convert(npcMemberGroup.getUid(),npcMemberGroup.getName(),levelDto.getLevel(),npcMemberGroup.getCreateTime());
-                List<MemberUnitVo> members = npcMemberGroup.getMembers().stream().filter(member -> member.getStatus().equals(StatusEnum.ENABLED.getValue()) && !member.getIsDel()).map(member -> MemberUnitVo.convert(member.getUid(),member.getName(),levelDto.getLevel(),member.getCreateTime())).sorted(Comparator.comparing(MemberUnitVo::getCreateTime)).collect(Collectors.toList());
-                memberUnitVo.setChildren(members);
-                MemberUnitVos.add(memberUnitVo);
+                if (systemSetting.getVoterOpinionToAll() || (!systemSetting.getVoterOpinionToAll() && account.getVoter().getVillage().getNpcMemberGroup().getUid().equals(npcMemberGroup.getUid()))){
+                    MemberUnitVo memberUnitVo = MemberUnitVo.convert(npcMemberGroup.getUid(),npcMemberGroup.getName(),levelDto.getLevel(),npcMemberGroup.getCreateTime());
+                    List<MemberUnitVo> members = npcMemberGroup.getMembers().stream()
+                            .filter(member -> member.getStatus().equals(StatusEnum.ENABLED.getValue()) && !member.getIsDel() && !member.getType().equals(specialMan.getUid()) && (systemSetting.getVoterOpinionToAll() || (!systemSetting.getVoterOpinionToAll() && account.getVoter().getVillage().getNpcMemberGroup().getUid().equals(member.getNpcMemberGroup().getUid()))))
+                            .map(member -> MemberUnitVo.convert(member.getUid(),member.getName(),levelDto.getLevel(),member.getCreateTime()))
+                            .sorted(Comparator.comparing(MemberUnitVo::getCreateTime)).collect(Collectors.toList());
+                    memberUnitVo.setChildren(members);
+                    MemberUnitVos.add(memberUnitVo);
+                }
             }
             MemberUnitVo memberUnitVo = MemberUnitVo.convert(townUid,"区代表",LevelEnum.AREA.getValue(),new Date());
             List<NpcMember> npcMembers = npcMemberRepository.findByTownUidAndLevelAndIsDelFalse(townUid,LevelEnum.AREA.getValue());
-            memberUnitVo.setChildren(npcMembers.stream().filter(member -> member.getStatus().equals(StatusEnum.ENABLED.getValue())  && !member.getIsDel()).map(member -> MemberUnitVo.convert(member.getUid(),member.getName(),LevelEnum.AREA.getValue(),member.getCreateTime())).sorted(Comparator.comparing(MemberUnitVo::getCreateTime)).collect(Collectors.toList()));
+            memberUnitVo.setChildren(npcMembers.stream()
+                    .filter(member -> member.getStatus().equals(StatusEnum.ENABLED.getValue()) && !member.getIsDel() && !member.getType().equals(specialMan.getUid()))
+                    .map(member -> MemberUnitVo.convert(member.getUid(),member.getName(),LevelEnum.AREA.getValue(),member.getCreateTime()))
+                    .sorted(Comparator.comparing(MemberUnitVo::getCreateTime)).collect(Collectors.toList()));
             MemberUnitVos.add(0,memberUnitVo);
         }else{
             String areaUid = userDetails.getArea().getUid();
+            //开关判断能否给非本镇的代表提意见
+            systemSetting  = systemSettingRepository.findByLevelAndAreaUid(LevelEnum.AREA.getValue(),areaUid);
             Area area = areaRepository.findByUid(areaUid);
             Set<Town> towns = area.getTowns();
             for (Town town : towns) {
-                if (town.getStatus().equals(StatusEnum.ENABLED.getValue()) && !town.getIsDel()) {
+                if (town.getStatus().equals(StatusEnum.ENABLED.getValue()) && !town.getIsDel()  && (systemSetting.getVoterOpinionToAll() || (!systemSetting.getVoterOpinionToAll() && account.getVoter().getTown().getUid().equals(town.getUid())))) {
                     MemberUnitVo memberUnitVo = MemberUnitVo.convert(town.getUid(), town.getName(), levelDto.getLevel(), town.getCreateTime());
-                    List<MemberUnitVo> members = town.getNpcMembers().stream().filter(member -> member.getLevel().equals(LevelEnum.AREA.getValue()) && member.getStatus().equals(StatusEnum.ENABLED.getValue()) && !member.getIsDel()).map(member -> MemberUnitVo.convert(member.getUid(), member.getName(), levelDto.getLevel(), member.getCreateTime())).sorted(Comparator.comparing(MemberUnitVo::getCreateTime)).collect(Collectors.toList());
+                    List<MemberUnitVo> members = town.getNpcMembers().stream()
+                            .filter(member -> member.getLevel().equals(LevelEnum.AREA.getValue()) && member.getStatus().equals(StatusEnum.ENABLED.getValue()) && !member.getIsDel() && !member.getType().equals(specialMan.getUid()) && (systemSetting.getVoterOpinionToAll() || (!systemSetting.getVoterOpinionToAll() && account.getVoter().getTown().getUid().equals(member.getTown().getUid()))))
+                            .map(member -> MemberUnitVo.convert(member.getUid(), member.getName(), levelDto.getLevel(), member.getCreateTime()))
+                            .sorted(Comparator.comparing(MemberUnitVo::getCreateTime)).collect(Collectors.toList());
                     memberUnitVo.setChildren(members);
                     MemberUnitVos.add(memberUnitVo);
                 }
@@ -147,12 +172,14 @@ public class NpcMemberServiceImpl implements NpcMemberService {
 
         RespBody body = new RespBody();
         //其它查询条件
+        NpcMemberRole specialMan = npcMemberRoleRepository.findByKeyword(NpcMemberRoleEnum.SPECIAL_MAN.getKeyword());
         Specification<NpcMember> spec = (root, query, cb) -> {
             List<Predicate> predicateList = new ArrayList<>();
             //查询与bgAdmin同级的代表
             predicateList.add(cb.equal(root.get("level"), level));
             predicateList.add(cb.isFalse(root.get("isDel")));
             predicateList.add(cb.equal(root.get("status").as(Byte.class), StatusEnum.ENABLED.getValue()));
+            predicateList.add(cb.notEqual(root.get("type").as(String.class), specialMan.getUid()));
             //同镇的代表 or 同区的代表
             if (level.equals(LevelEnum.TOWN.getValue())) {
                 predicateList.add(cb.equal(root.get("npcMemberGroup").get("uid"), uid));
