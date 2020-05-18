769f5263ab221b3350ccc1a24f5f67a89984451c
really <renfaxin@qq.com>
2020-05-18 10:36:32 +0800

    持久层提交

---
+++ repository/src/main/java/com/cdkhd/npc/repository/suggestion_deal/SuggestionSettingRepository.java
@@ -0,0 +1,9 @@
+package com.cdkhd.npc.repository.suggestion_deal;
+
+import com.cdkhd.npc.entity.SuggestionSetting;
+import com.cdkhd.npc.repository.base.BaseRepository;
+
+public interface SuggestionSettingRepository extends BaseRepository<SuggestionSetting> {
+
+
+}
