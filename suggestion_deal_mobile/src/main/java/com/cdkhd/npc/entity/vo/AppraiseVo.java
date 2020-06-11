fa51f527f9eebb784e058ef27a4efe5f600f20e8
liyang <liyang96615@163.com>
2020-06-11 18:35:03 +0800

    评价vo

---
+++ suggestion_deal_mobile/src/main/java/com/cdkhd/npc/entity/vo/AppraiseVo.java
@@ -0,0 +1,35 @@
+package com.cdkhd.npc.entity.vo;
+/*
+ * @description:建议评价
+ * @author:liyang
+ * @create:2020-06-05
+ */
+
+import com.cdkhd.npc.entity.Appraise;
+import com.cdkhd.npc.vo.BaseVo;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+@Setter
+@Getter
+public class AppraiseVo extends BaseVo {
+
+    //办理结果评分
+    private Byte result;
+
+    //办理态度评分
+    private Byte attitude;
+
+    //评价人姓名
+    private String npcMemberName;
+
+    //原因
+    private String reason;
+
+    public static AppraiseVo convert(Appraise appraise) {
+        AppraiseVo appraiseVo = new AppraiseVo();
+        BeanUtils.copyProperties(appraise, appraiseVo);
+        return appraiseVo;
+    }
+}
