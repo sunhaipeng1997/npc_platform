1b4954814cd2943fb9f6eb9d3c05d30f8e37c631
arya <mrliuyon@163.com>
2020-06-18 17:36:41 +0800

    大数据平台后台接口调整完善

--- repository/src/main/java/com/cdkhd/npc/repository/member_house/SuggestionRepository.java
+++ repository/src/main/java/com/cdkhd/npc/repository/member_house/SuggestionRepository.java
@@ -16,6 +16,26 @@ public interface SuggestionRepository extends BaseRepository<Suggestion> {
 
     List<Suggestion> findByRaiserUid(String npcUid);
 
+    List<Suggestion> findBySuggestionBusinessUid(String businessUid);
+
+    List<Suggestion> findBySuggestionBusinessUidAndStatusGreaterThanAndLevel(String businessUid, Byte status, Byte level);
+
+    //统计该镇办理中的所有建议数量（包括状态为“办完”但还未“办结”的）
+    @Query(value = "select  count (sug.uid) from Suggestion as sug where sug.town.uid = ?1 and sug.level = ?2 and sug.status in (3, 4, 5, 6)")
+    Integer countTownDoingSugNum(String uid, Byte value);
+
+    //统计该镇办结的所有建议数量
+    @Query(value = "select  count (sug.uid) from Suggestion as sug where sug.town.uid = ?1 and sug.level = ?2 and sug.status in (7, 8)")
+    Integer countTownFinishSugNum(String uid, Byte value);
+
+    //统计该小组办理中的所有建议数量
+    @Query(value = "select  count (sug.uid) from Suggestion as sug where sug.raiser.npcMemberGroup.uid = ?1 and sug.level = ?2 and sug.status in (3, 4, 5, 6)")
+    Integer countGroupDoingSugNum(String uid, Byte value);
+
+    //统计该小组办结的所有建议数量
+    @Query(value = "select  count (sug.uid) from Suggestion as sug where sug.raiser.npcMemberGroup.uid = ?1 and sug.level = ?2 and sug.status in (7, 8)")
+    Integer countGroupFinishSugNum(String uid, Byte value);
+
     @Query(value = "select count(sug.uid) from Suggestion as sug where sug.createTime >= ?1 and sug.level = ?2 and sug.town.uid = ?3")
     Integer countTownTodayNumber(Date today, Byte level, String uid);
 
@@ -23,17 +43,60 @@ public interface SuggestionRepository extends BaseRepository<Suggestion> {
     Integer countAreaTodayNumber(Date today, Byte level, String uid);
 
     //由于JPA查询结果只能映射实体类，故需要在sql中使用 new 语法
-    @Query("select new com.cdkhd.npc.vo.CountVo(town.name, count(sug.uid)) from Suggestion sug, Town town " +
-            "where sug.town=town.id and sug.area.id=?1 and sug.isDel=false and sug.level=1 and sug.status>=3 " +
-            "group by sug.town")
-    List<CountVo> count4Town(Long areaId);
+    @Query("select new com.cdkhd.npc.vo.CountVo(sug.town.name, count(sug.uid)) from Suggestion sug " +
+            "where sug.area.id=?1 and sug.isDel=false and sug.level=2 and sug.status>=3 " +
+            "group by sug.town.id")
+    List<CountVo> countByArea(Long areaId);
 
-    @Query("select new com.cdkhd.npc.vo.CountVo(stype.name, count(sug.uid)) from Suggestion sug, SuggestionBusiness stype " +
-            "where sug.suggestionBusiness=stype.id and sug.area.id=?1 and sug.isDel=false and sug.level=1 and sug.status>=3 " +
-            "group by sug.suggestionBusiness")
-    List<CountVo> count4Type(Long areaId);
+    @Query("select new com.cdkhd.npc.vo.CountVo(sug.raiser.npcMemberGroup.name, count(sug.uid)) from Suggestion sug " +
+            "where sug.isDel=false and sug.level=1 and sug.area.id=?1 and sug.town.uid=?2 and sug.status>=3 " +
+            "group by sug.raiser.npcMemberGroup.id")
+    List<CountVo> countByTown(Long areaId, String townUid);
+
+    @Query("select new com.cdkhd.npc.vo.CountVo(sug.suggestionBusiness.name, count(sug.uid)) from Suggestion sug " +
+            "where sug.area.id=?1 and sug.isDel=false and sug.level=2 and sug.status>=3 " +
+            "group by sug.suggestionBusiness.id")
+    List<CountVo> countByAreaType(Long areaId);
+
+    @Query("select new com.cdkhd.npc.vo.CountVo(sug.suggestionBusiness.name, count(sug.uid)) from Suggestion sug " +
+            "where sug.isDel=false and sug.level=1 and sug.area.id=?1 and sug.town.uid=?2 and sug.status>=3 " +
+            "group by sug.suggestionBusiness.id")
+    List<CountVo> countByTownType(Long areaId, String townUid);
 
     @Query("select count(sug.uid) from Suggestion sug " +
-            "where sug.area.id=?1 and sug.isDel=false and sug.status>=3 ")
-    Integer countAll(Long areaId);
+            "where sug.isDel=false and sug.level=2 and sug.area.id=?1 and sug.status>=3 ")
+    Integer countAll4Area(Long areaId);
+
+    @Query("select count(sug.uid) from Suggestion sug " +
+            "where sug.isDel=false and sug.level=1 and sug.area.id=?1 and sug.town.uid=?2 and sug.status>=3 ")
+    Integer countAll4Town(Long areaId, String townUid);
+
+    @Query(value = "select count(sug.uid) from Suggestion as sug where sug.auditTime >= ?1 and sug.level = ?2 and sug.town.uid = ?3")
+    Integer countTownMonthNewNumber(Date date, Byte level, String uid);
+
+    @Query(value = "select count(sug.uid) from Suggestion as sug where sug.auditTime >= ?1 and sug.level = ?2 and sug.area.uid = ?3")
+    Integer countAreaMonthNewNumber(Date date, Byte level, String uid);
+
+    @Query(value = "select count(sug.uid) from Suggestion as sug where sug.accomplishTime >= ?1 and sug.level = ?2 and sug.town.uid = ?3")
+    Integer countTownMonthCompletedNumber(Date date, Byte level, String uid);
+
+    @Query(value = "select count(sug.uid) from Suggestion as sug where sug.accomplishTime >= ?1 and sug.level = ?2 and sug.area.uid = ?3")
+    Integer countAreaMonthCompletedNumber(Date date, Byte level, String uid);
+
+    @Query(value = "select count(sug.uid) from Suggestion as sug where sug.level = ?1 and sug.town.uid = ?2 and (sug.status = 4 or sug.status = 5 or sug.status = 6)")
+    Integer countTownDealingNumber(Byte level, String uid);
+
+    @Query(value = "select count(sug.uid) from Suggestion as sug where sug.level = ?1 and sug.area.uid = ?2 and (sug.status = 4 or sug.status = 5 or sug.status = 6)")
+    Integer countAreaDealingNumber(Byte level, String uid);
+
+    //镇人大后台管理员查看本月新增的建议
+    @Query(value = "select count(sug.uid) from Suggestion as sug where sug.raiseTime >= ?1 and sug.raiseTime < ?2 and sug.level = ?3 and sug.town.uid = ?4 and sug.status not in (-1, 0, 1, 2)")
+    Integer adminCountTownMonthNewNumber(Date start, Date end, Byte level, String uid);
+
+    //区人大后台管理员查看本月新增的建议
+    @Query(value = "select count(sug.uid) from Suggestion as sug where sug.raiseTime >= ?1 and sug.raiseTime < ?2 and sug.level = ?3 and sug.area.uid = ?4 and sug.status not in (-1, 0, 1, 2)")
+    Integer adminCountAreaMonthNewNumber(Date start, Date end, Byte level, String uid);
+
+    List<Suggestion> findByExpectDateIsNotNullAndFinishTimeIsNullAndIsDelFalse();
+
 }
