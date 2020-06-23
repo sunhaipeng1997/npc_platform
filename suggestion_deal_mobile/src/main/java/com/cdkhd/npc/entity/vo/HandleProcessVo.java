6912534257f0d74e25aa1faaccb91ab2439adb7a
liyang <liyang96615@163.com>
2020-06-23 11:42:03 +0800

    关联办理流程的图片

--- suggestion_deal_mobile/src/main/java/com/cdkhd/npc/entity/vo/HandleProcessVo.java
+++ suggestion_deal_mobile/src/main/java/com/cdkhd/npc/entity/vo/HandleProcessVo.java
@@ -1,8 +1,6 @@
 package com.cdkhd.npc.entity.vo;
 
 import com.cdkhd.npc.entity.HandleProcess;
-import com.cdkhd.npc.entity.UnitImage;
-import com.cdkhd.npc.enums.ImageTypeEnum;
 import com.cdkhd.npc.vo.BaseVo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
@@ -16,6 +14,7 @@ import java.util.stream.Collectors;
 @Getter
 @Setter
 public class HandleProcessVo extends BaseVo {
+
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+08")
     private Date handleTime;
 
@@ -26,7 +25,7 @@ public class HandleProcessVo extends BaseVo {
     public static HandleProcessVo convert(HandleProcess hp) {
         HandleProcessVo vo = new HandleProcessVo();
         BeanUtils.copyProperties(hp, vo);
-        vo.setImages(hp.getProcessImages().stream().filter(uImg -> uImg.getType().equals(ImageTypeEnum.HANDLE_PROCESS.getValue())).map(UnitImage::getUrl).collect(Collectors.toList()));
+        vo.setImages(hp.getProcessImages().stream().map(processImage -> processImage.getUrl()).collect(Collectors.toList()));
         return vo;
     }
 }
